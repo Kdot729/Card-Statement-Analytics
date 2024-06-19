b0c8958c8f1578b6150ae07e5f93f666a58b0a03
Kdot729 <110777824+Kdot729@users.noreply.github.com>
2024-06-19 13:07:38 -0400

    Implemented axios post request

--- frontend/app/(tabs)/index.jsx
+++ frontend/app/(tabs)/index.jsx
@@ -1,8 +1,9 @@
 
 import {View, Text, Button, StyleSheet} from 'react-native';
 import React, { useState } from 'react';
-import {DocumentPicker} from 'expo-document-picker';
+import * as DocumentPicker from 'expo-document-picker';
 import File_Upload from '../components/file_upload';
+import { AxiosInstance } from "../../scripts/axios_interceptor";
 
 export default function App() 
 {
@@ -11,8 +12,16 @@ export default function App()
 
     const Upload_File = async () => 
     {
-        const Document = await DocumentPicker.getDocumentAsync({type: 'application/pdf'})
-        Set_Document(Document)
+        const PDF_Document = await DocumentPicker.getDocumentAsync({type: 'application/pdf'})
+        Set_Document(PDF_Document)
+
+        const URI = PDF_Document.assets[0].uri
+        const No_Header_URI = URI.split(",")[1]
+        const URI_Last_Characters = No_Header_URI.substr(No_Header_URI.length - 16);
+        const URI_ID = URI_Last_Characters.slice(0,-1)
+
+        const Post_Endpoint = `post/${URI_ID}`
+        await AxiosInstance.post(Post_Endpoint, {URI})
     }
 
     return  (
