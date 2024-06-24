b4c57363382a304988d8c524ab299d2b6175d9c2
Kdot729 <110777824+Kdot729@users.noreply.github.com>
2024-06-24 13:51:31 -0400

    Added models
    Added Transaction_Average and Analytics model

--- backend/database/models.py
+++ backend/database/models.py
@@ -1,1 +1,10 @@
 from pydantic import BaseModel
+
+class Transaction_Average(BaseModel):
+    Transaction: str
+    Amount: float
+
+class Analytics(BaseModel):
+    URI: str
+    URI_ID: str
+    Avg: list[Transaction_Average]
