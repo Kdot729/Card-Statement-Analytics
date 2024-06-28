295b5d49c2ba892e0447a5fb9b88e5fe345bf636
Kdot729 <110777824+Kdot729@users.noreply.github.com>
2024-06-28 12:03:32 -0400

    Updated post method to use newly created classes

--- backend/main.py
+++ backend/main.py
@@ -2,8 +2,9 @@ from fastapi import FastAPI, APIRouter, Request
 from fastapi.middleware.cors import CORSMiddleware
 from backend.database.serializers import Analytics_Deserializer
 from backend.database.settings import API_Collection
-from backend.dataframe.dataframe import Dataframe
 import json
+from backend.dataframe.statistic import Statistic
+from backend.dataframe.transaction import Transaction
 from backend.extract_pdf.extract import Extract
 
 app = FastAPI()
@@ -27,14 +28,17 @@ async def Post_PDF(request: Request):
     PDF_ID = (await request.json())["PDF_ID"]
     Data = {"PDF_ID": PDF_ID}
     PDF = (await request.json())["PDF"]
-    
+
     if API_Collection.count_documents(Data, limit = 1) == 0:
 
         PDF_Object = Extract(PDF_ID, PDF)
-        Dataframe_Object = Dataframe(PDF_Object.Text_Array)
-        Data["Avg"] = Dataframe_Object.Avg
+        Transaction_Object = Transaction(PDF_Object.Text_Array)
+        Statistic_Object = Statistic(Transaction_Object.Dataframe)
+
+        Data["Mean"] = Statistic_Object.Mean
         response = API_Collection.insert_one(Data)
         # print("ID:", response.inserted_id)
+
     else:
         pass
     
