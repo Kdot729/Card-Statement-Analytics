2b1f43ab73cfea3677328a8904d89c1773de33b0
Kdot729 <110777824+Kdot729@users.noreply.github.com>
2024-06-28 06:54:23 -0400

    Implemented dataframe getter

--- backend/dataframe/dataframe.py
+++ backend/dataframe/dataframe.py
@@ -9,19 +9,23 @@ panda.set_option('display.max_colwidth', None)
 class Dataframe():
 
     def __init__(self, Dataframe: panda.DataFrame) -> None:
-        self.Dataframe = Dataframe
+        self._Dataframe = Dataframe
 
     def Trim_All_Columns(self, Columns: list[str]) -> None:
-        self.Dataframe[Columns] = self.Dataframe[Columns].apply(lambda row: row.str.strip())
+        self._Dataframe[Columns] = self._Dataframe[Columns].apply(lambda row: row.str.strip())
 
     def Removing_Character(self, Column: str, Remove_Character=" ", New_Character="") -> None:
-        self.Dataframe[Column] = self.Dataframe[Column].str.replace(Remove_Character, New_Character)
+        self._Dataframe[Column] = self._Dataframe[Column].str.replace(Remove_Character, New_Character)
 
     def Group_By(self, Grouped_Column: str):
-        return self.Dataframe.groupby(Grouped_Column)
+        return self._Dataframe.groupby(Grouped_Column)
 
     def Merging_Dataframes(self, On_Merge_Column: str, Dataframe_List: list[panda.DataFrame]) -> panda.DataFrame:
 
         Merged_Function: partial[panda.DataFrame] = partial(panda.merge, on=On_Merge_Column, how='outer')
         return reduce(Merged_Function, Dataframe_List)
+    
+    @property
+    def Dataframe(self):
+        return self._Dataframe
     
