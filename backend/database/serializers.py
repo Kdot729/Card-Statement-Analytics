471adee3e86dee323d7d182f8a03ba8e22f7c06e
Kdot729 <110777824+Kdot729@users.noreply.github.com>
2024-06-28 17:32:03 -0400

    Added another field for serializer

--- backend/database/serializers.py
+++ backend/database/serializers.py
@@ -1,6 +1,7 @@
 def Analytics_Serializer(Analytic):
     return {
             "PDF_ID": Analytic["PDF_ID"],
+            "Activity_Period": Analytic["Activity_Period"],
             "Mean": Analytic["Mean"]
             }
 
