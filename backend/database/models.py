1e9659d0fcad7c5cf60e7745ea577c3f9038c89d
Kdot729 <110777824+Kdot729@users.noreply.github.com>
2024-06-29 06:37:49 -0400

    Deleted Transaction_Average model
    The fields in Transaction_Average are now in Statistic

--- backend/database/models.py
+++ backend/database/models.py
@@ -1,13 +1,8 @@
 from pydantic import BaseModel
 
-class Transaction_Average(BaseModel):
-    Transaction: str
-    Amount: float
-
 class Analytics(BaseModel):
     PDF_ID: str
     Activity_Period: str
-    # Mean: list[Transaction_Average]
     Statistic: list[Statistic]
 
 class Statistic:
