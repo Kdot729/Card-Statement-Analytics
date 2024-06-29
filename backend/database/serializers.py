52b6c53be08657a2b4209e1ffae4a529e2e49bcd
Kdot729 <110777824+Kdot729@users.noreply.github.com>
2024-06-29 07:59:42 -0400

    Updated serializer

--- backend/database/serializers.py
+++ backend/database/serializers.py
@@ -2,7 +2,7 @@ def Analytics_Serializer(Analytic):
     return {
             "PDF_ID": Analytic["PDF_ID"],
             "Activity_Period": Analytic["Activity_Period"],
-            "Mean": Analytic["Mean"]
+            "Statistic": Analytic["Statistic"]
             }
 
 def Analytics_Deserializer(Analytics):
