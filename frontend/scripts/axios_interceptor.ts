7ceef030a5186c0dc762f6eb3cd0bb890f423b7f
Kdot729 <110777824+Kdot729@users.noreply.github.com>
2024-06-30 19:00:37 -0400

    Changed url for axios
    - Android doesn't use 127.0.0.1 for API
    - Added comments about Android and web URLs

--- frontend/scripts/axios_interceptor.ts
+++ frontend/scripts/axios_interceptor.ts
@@ -1,6 +1,11 @@
 import axios from 'axios'
 
-const Base_Url = 'http://127.0.0.1:8000/'
+//Note Mobile URL is http://10.0.2.2:8000/'
+//Note Web URL is 'http://10.0.2.2:8000/'
+
+const Base_Url = 'http://10.0.2.2:8000/'
+// const Base_Url = 'http://127.0.0.1:8000/'
+
 export const AxiosInstance = axios.create({
     baseURL: Base_Url, 
     timeout: 30000, 
