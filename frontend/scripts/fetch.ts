4142326587fb992d480b66770f70dbbb0cac1bac
Kdot729 <110777824+Kdot729@users.noreply.github.com>
2024-07-01 10:51:08 -0400

    Fixed useFetch
    - Added type annotation for parameter
    - Changed URL of get request
    - Removed parameter for Get_Request

--- frontend/scripts/fetch.ts
+++ frontend/scripts/fetch.ts
@@ -1,19 +1,15 @@
 import { useQuery } from "react-query";
 import axios from "axios";
 
-export const useFetch = (PDF_ID) =>
+export const useFetch = (PDF_ID: string) =>
 {
     const Get_Request = async () =>
     {
-    
-        const Domain_Name = window.location.hostname
-        const {data} = await axios.get(`http://${Domain_Name}:8000/get/PDF/${PDF_ID}`)
-    
+        const {data} = await axios.get(`http://10.0.2.2:8000/get/PDF/${PDF_ID}`)
         return await data
-    
     }
 
-    const {data, status} = useQuery("Fetch", () => Get_Request(PDF_ID))
+    const {data, status} = useQuery("Fetch", () => Get_Request())
 
     return status != "success" ? [[]] : data
 }
