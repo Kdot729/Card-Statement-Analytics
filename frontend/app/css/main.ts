0776396bbe31357a108deb916dcf41389308edb8
Kdot729 <110777824+Kdot729@users.noreply.github.com>
2024-07-07 09:50:07 -0400

    Created file for css

---
+++ frontend/app/css/main.ts
@@ -0,0 +1,8 @@
+import {StyleSheet} from 'react-native';
+
+const CSS = StyleSheet.create(
+{
+    Center: {justifyContent: "center", alignItems: "center"}
+})
+
+export const Center = CSS["Center"]
