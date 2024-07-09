c13634391b83ace5a107cbf6cbebd91c0ddb3b99
Kdot729 <110777824+Kdot729@users.noreply.github.com>
2024-07-09 11:48:17 -0400

    Added reducer to store

--- frontend/scripts/redux/store.ts
+++ frontend/scripts/redux/store.ts
@@ -1,8 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit'
+import Sorting_Reducer  from './sort'
 
 export const store = configureStore(
 {
-    reducer: {},
+    reducer: {Sorting: Sorting_Reducer},
 })
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
