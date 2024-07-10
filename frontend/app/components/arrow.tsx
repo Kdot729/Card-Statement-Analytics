8489b5f473e218010e27e3d05f291a116abd20d5
Kdot729 <110777824+Kdot729@users.noreply.github.com>
2024-07-10 07:01:31 -0400

    Updated arrow icons
    - Created dynamics refs
    - Changed dispatch method
    - Changed translating y coordinate

--- frontend/app/components/arrow.tsx
+++ frontend/app/components/arrow.tsx
@@ -1,33 +1,36 @@
 import {View, StyleSheet} from 'react-native';
 import { FontAwesome } from '@expo/vector-icons';
-import React, { useRef } from "react";
+import React, { createRef, useRef } from "react";
 import { Center } from '../css/main';
 import { useDispatch, useSelector } from 'react-redux';
-import { Ascending, Descending, Unsorted } from '@/scripts/redux/sort';
+import { Sorted, Unsorted } from '@/scripts/redux/sort';
 
 const Arrow = ({Column}): JSX.Element =>
 {
-    const Column_Ref = useRef(null)
+    //Note There's 2 createRef because each column has 2 arrows
+    const Column_Ref = useRef([createRef(), createRef()]);
+
     const { value } = useSelector((state) => state["Sorting"])
     const Dispatch = useDispatch()
 
     const Arrow_Size = 24
-    const Move_Vertically = 10.5
+    const Move_Vertically = 11
     const Arrow_Color = "rgb(7, 21, 177)"
 
-    const Dispatching = (Sort_Function) => Dispatch(Sort_Function(Column_Ref.current.props.id))
+    const Arrows = [{Direction: "sort-up", Transalte_Y: Move_Vertically, Sort: "Ascending"}, 
+                    {Direction: "sort-down", Transalte_Y: -Move_Vertically, Sort: "Descending"}]
 
-    const Arrows = [{Direction: "sort-up", Transalte_Y: Move_Vertically, Sort: "Ascending", Press: () => Dispatching(Ascending)}, 
-                    {Direction: "sort-down", Transalte_Y: -Move_Vertically, Sort: "Descending", Press: () => Dispatching(Descending)}]
-
-    const Font_Awesome_Arrows = Arrows.map((Arrow) =>
+    const Font_Awesome_Arrows = Arrows.map((Arrow, Index) =>
     {
         const Transform = StyleSheet.create({Transform: {transform: `translate(3px, ${Arrow["Transalte_Y"]}px)`}})["Transform"]
         const Direction = Arrow["Direction"]
-        const Sort = Arrow["Press"]
+
+        const Ref = Column_Ref.current[Index]
+        const Sort = () => Dispatch(Sorted(Ref.current.props.id))
+        
         const ID = `${Column}-${Arrow["Sort"]}`
 
-        return <FontAwesome id={ID} key={ID} ref={Column_Ref} style={Transform} name={Direction}  
+        return <FontAwesome id={ID} key={ID} ref={Ref} style={Transform} name={Direction}  
                     size={Arrow_Size} color={Arrow_Color} onPress={Sort}/>
     })
 
