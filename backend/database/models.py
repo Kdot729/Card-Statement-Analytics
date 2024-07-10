e30a9b160587f6d51f41517681a74cbc18af9c67
Kdot729 <110777824+Kdot729@users.noreply.github.com>
2024-07-10 08:28:58 -0400

    Implemented case insensitive enum

--- backend/database/models.py
+++ backend/database/models.py
@@ -1,3 +1,4 @@
+from enum import StrEnum, auto
 from pydantic import BaseModel
 
 class Statistic(BaseModel):
@@ -12,3 +13,18 @@ class Analytics(BaseModel):
     PDF_ID: str
     Activity_Period: str
     Statistic: list[Statistic]
+
+class Sorting(StrEnum):
+
+    #Note auto() converts the field to lowercase
+    Ascending = auto()
+    Descending = auto()
+
+    #Note Case insensitive
+    @classmethod
+    def _missing_(cls, Value):
+        Value = Value.lower()
+        for Field in cls:
+            if Field == Value:
+                return Field
+        return None
