201332669844ff5eb8ef919f535f434a8e23eb4b
Kdot729 <110777824+Kdot729@users.noreply.github.com>
2024-07-10 08:28:07 -0400

    Updated model inheritance

--- backend/database/models.py
+++ backend/database/models.py
@@ -1,14 +1,14 @@
 from pydantic import BaseModel
 
-class Analytics(BaseModel):
-    PDF_ID: str
-    Activity_Period: str
-    Statistic: list[Statistic]
-
-class Statistic:
+class Statistic(BaseModel):
     ID: str
     Mean: float
     Max: float
     Min: float
     Freq: int
     Range: float
+
+class Analytics(BaseModel):
+    PDF_ID: str
+    Activity_Period: str
+    Statistic: list[Statistic]
