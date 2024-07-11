cf6245b056276fe1e2c95e346a6671554476b21a
Kdot729 <110777824+Kdot729@users.noreply.github.com>
2024-07-11 09:57:39 -0400

    Created table flatlist component

---
+++ frontend/app/components/table/flatlist.tsx
@@ -0,0 +1,19 @@
+import { View, Text, FlatList } from 'react-native'
+import React from 'react'
+import Table_Header from './header'
+import Table_Body from './body'
+import { useFetch } from '@/scripts/fetch'
+import { Use_Context, Create_Context } from '@/scripts/hook/context'
+import { useSelector } from 'react-redux'
+
+const Table_Flatlist = () => 
+{
+    const PDF_ID = Use_Context(Create_Context)
+    const { URL } = useSelector((state) => state["Sorting"])
+    const {Statistic} = useFetch(URL, PDF_ID)
+
+    return <FlatList data={Statistic} ListHeaderComponent={<Table_Header />} 
+        keyExtractor={(Transaction) => Transaction["ID"]} renderItem={Table_Body} />
+}
+
+export default Table_Flatlist
