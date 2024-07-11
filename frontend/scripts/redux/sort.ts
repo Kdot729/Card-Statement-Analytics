818f286bc5eb8bde09b7c3e5dda851cc0c2301f1
Kdot729 <110777824+Kdot729@users.noreply.github.com>
2024-07-11 10:01:50 -0400

    Changed reducer
    - Changed initial state
    - Changed sorted reducer

--- frontend/scripts/redux/sort.ts
+++ frontend/scripts/redux/sort.ts
@@ -1,7 +1,7 @@
 import { createSlice } from '@reduxjs/toolkit'
 import type { PayloadAction } from '@reduxjs/toolkit'
 
-const initialState = {value: "Unsorted"}
+const initialState = {URL: "get/PDF"}
 
 export const Sort_Slice = createSlice(
 {
@@ -10,15 +10,20 @@ export const Sort_Slice = createSlice(
     initialState,
     reducers:   {
                     Unsorted: (state, action) => {},
-                    Sorted: (state, action) => {Sort_Table(action.payload)}
+                    Sorted: (state, action) => 
+                    {
+                        state.URL = Sorted_URL(action.payload)
+                    }
                 },
 })
 
-const Sort_Table = (ID) => 
+const Sorted_URL = (ID) => 
 {
     const Split_ID: string = ID.split("-");
     const Column: string = Split_ID[0]
     const Sort_Direction: string = Split_ID[1].toLocaleLowerCase()
+
+    return `get/${Sort_Direction}/${Column}/PDF`
 }
 
 export const { Sorted, Unsorted } = Sort_Slice.actions
