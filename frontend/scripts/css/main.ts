925ac99d6d48fde5db44e6b12eb3d9318f4d9da8
Kdot729 <110777824+Kdot729@users.noreply.github.com>
2024-07-11 08:00:40 -0400

    Moved css to this file

--- frontend/scripts/css/main.ts
+++ frontend/scripts/css/main.ts
@@ -6,3 +6,13 @@ const CSS = StyleSheet.create(
 })
 
 export const Center = CSS["Center"]
+
+export const Font_Size = (Size: number) =>
+{
+    return StyleSheet.create({Font_Size: {fontSize: Size}})["Font_Size"]
+}
+
+export const Margin = (Margin: number) =>
+{
+    return StyleSheet.create({Margin: {margin: Margin}})["Margin"]
+}
