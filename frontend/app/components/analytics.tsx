5506e48debc7b0fb52b5fd892ed859f7a30224a8
Kdot729 <110777824+Kdot729@users.noreply.github.com>
2024-07-11 13:28:10 -0400

    Created table wrapper component

---
+++ frontend/app/components/analytics.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Table } from './table/table'
+
+const Analytics = () => 
+{
+    return  <>
+            <Table />
+            </>
+
+}
+
+export default Analytics
--- frontend/app/components/file.tsx
+++ frontend/app/components/file.tsx
@@ -3,10 +3,10 @@ import {View, Text, Button, StyleSheet} from 'react-native';
 import { useState } from 'react';
 import * as DocumentPicker from 'expo-document-picker';
 import { AxiosInstance } from "../../scripts/axios_interceptor";
-import { Table } from "./table/table";
 import { Create_Context } from "../../scripts/hook/context";
 import React from "react";
 import * as FileSystem from 'expo-file-system';
+import Analytics from "./analytics";
 
 const Upload = (properties) =>
 {
@@ -41,7 +41,7 @@ export const File = () =>
 
     return  PDF_ID ? 
             <Create_Context.Provider value={PDF_ID}>
-                <Table />
+                <Analytics />
             </Create_Context.Provider > : 
             <View>
                 <Text style={Styles.Header}>Upload PDF</Text>
