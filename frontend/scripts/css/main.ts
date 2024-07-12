9326d3c77cc8616e325a77b32312989bb2160e6c
Kdot729 <110777824+Kdot729@users.noreply.github.com>
2024-07-12 15:59:41 -0400

    Updated css
    - Changed parameter name
    - Implemented function for flex

--- frontend/scripts/css/main.ts
+++ frontend/scripts/css/main.ts
@@ -12,7 +12,12 @@ export const Font_Size = (Size: number) =>
     return StyleSheet.create({Font_Size: {fontSize: Size}})["Font_Size"]
 }
 
-export const Margin = (Margin: number) =>
+export const Margin = (Margin_Number: number) =>
 {
-    return StyleSheet.create({Margin: {margin: Margin}})["Margin"]
+    return StyleSheet.create({Margin: {margin: Margin_Number}})["Margin"]
+}
+
+export const Flex = (Flex_Number: number) =>
+{
+    return StyleSheet.create({Flex: {flex: Flex_Number}})["Flex"]
 }
