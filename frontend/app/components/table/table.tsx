818e8a5a9e41c15012f3d13d642e4d793e7fea44
Kdot729 <110777824+Kdot729@users.noreply.github.com>
2024-07-12 16:42:19 -0400

    Updated table component
    - Removed css function
    - Removed flatlist's view

--- frontend/app/components/table/table.tsx
+++ frontend/app/components/table/table.tsx
@@ -1,21 +1,14 @@
-import {View, StyleSheet} from 'react-native';
+import {View} from 'react-native';
 import React from "react";
 import Table_Date from './date';
-import { Center, Margin } from '@/scripts/css/main';
+import { Center, Flex } from '@/scripts/css/main';
 import Table_Flatlist from './flatlist';
 
 export const Table = () =>
 {
-    const Container_Styles = [Flex, Center]
+    const Container_Styles = [Flex(1), Center]
     return  <View style={Container_Styles}>
                 <Table_Date />
-                <View style={Margin(3)}>
-                    <Table_Flatlist />
-                </View>
+                <Table_Flatlist />
             </View>
 }
-
-const Flex = StyleSheet.create(
-{
-    Container: {flex: 1},
-})["Container"]
