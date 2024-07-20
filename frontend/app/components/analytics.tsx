32ef315283594a21c0e6072c3c787463fdb2a786
Kdot729 <110777824+Kdot729@users.noreply.github.com>
2024-07-20 13:06:49 -0400

    Calling pie component

--- frontend/app/components/analytics.tsx
+++ frontend/app/components/analytics.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Table } from './table/table'
 import { ScrollView } from 'react-native'
+import Pie from './graphs/pie'
 
 const Analytics = () => 
 { 
     return  <ScrollView >
                 <Table />
+                <Pie></Pie>
             </ScrollView>
 }
 
