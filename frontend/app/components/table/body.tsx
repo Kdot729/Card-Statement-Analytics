b552b0679b769a90228b26d4931fb0b2031401ec
Kdot729 <110777824+Kdot729@users.noreply.github.com>
2024-07-22 09:00:51 -0400

    Update body.tsx
    - Changed parameter deconstructing
    - Passing dynamic color for background color

--- frontend/app/components/table/body.tsx
+++ frontend/app/components/table/body.tsx
@@ -4,17 +4,18 @@ import { Font_Size } from '@/scripts/css/main'
 import { Table_Element } from '@/scripts/css/table'
 import Table_Row from './row'
 
-const Table_Body = ({item}) => 
+const Table_Body = ({Row_Data, Row_Colors}) => 
 {
-    const Row = Object.entries(item).map(([Category, Value]) =>
+    const Row = Object.entries(Row_Data).map(([Category, Value]) =>
     {
         return <Table_Row key={Category}
             Component={<Text style={Font_Size(14)}>{Value}</Text>} />
     })
 
-    const Row_Container_Styles = [Body_Styles, Table_Element("rgb(228, 228, 228)")]
+    const Background_Color = Row_Colors[Row_Data["ID"]]
+    const Row_Container_Styles = [Body_Styles, Table_Element(Background_Color)]
 
-    return <View style={Row_Container_Styles} key={item["ID"]}>{Row}</View>
+    return <View style={Row_Container_Styles} key={Row_Data["ID"]}>{Row}</View>
 }
 
 const Body_Styles = StyleSheet.create(
