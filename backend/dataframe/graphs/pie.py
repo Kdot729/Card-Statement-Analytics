953c9146cdb9411a25e7f0a446b840b1a21648d1
Kdot729 <110777824+Kdot729@users.noreply.github.com>
2024-07-29 14:10:50 -0400

    Update pie.py
    - Refactor code because pie inherits from bar
    - Overrode Merge_Color_Dateframe

--- backend/dataframe/graphs/pie.py
+++ backend/dataframe/graphs/pie.py
@@ -1,29 +1,15 @@
 import pandas as panda
-from backend.dataframe.dataframe import Dataframe
-from backend.dataframe.graphs.graph import Graph
+from backend.dataframe.graphs.bar import Bar
 from backend.dataframe.transaction import Transaction
 
-class Pie(Graph):
+class Pie(Bar):
 
     def __init__(self, Records, Transaction_Colors):
 
-        super().__init__(panda.DataFrame.from_dict(Records))
-        Dataframe.Change_Column_Type(self, Transaction.Amount_Column)
-        Dataframe.Removing_Character(self, Transaction.Amount_Column, "$", "")
-        Dataframe.Change_Column_Type(self, Transaction.Amount_Column, float)
+        super().__init__(panda.DataFrame.from_dict(Records), Transaction_Colors)
 
-        Transaction_Sum = self._Dataframe.groupby([Transaction.ID_Column])[self.Amount_Column].sum().reset_index()
-        self.Pecentage_Transaction_Dataframe = Transaction.Calulcate_Sum_Percentage(self, Transaction_Sum)
-
-        self.Merge_Color_Dateframe(Transaction_Colors)
-        Dataframe.Convert_Dataframe_to_Dictionary(self, self.Pie_Dataframe)
-
-    def Merge_Color_Dateframe(self, Transaction_Colors):
-
-        Columns = [Transaction.ID_Column, Transaction.Color_Column]
-        Color_Dataframe = panda.DataFrame.from_dict(Transaction_Colors, orient="index").reset_index()
-        Color_Dataframe.columns = Columns
-        self.Pie_Dataframe = self.Pecentage_Transaction_Dataframe.merge(Color_Dataframe, on=Transaction.ID_Column, how='outer')
+    def Merge_Color_Dateframe(self):
+        Bar.Merge_Color_Dateframe(self)
 
         #Note Getting rid of Discover Payment so it doesn't show up in the pie graph
-        self.Pie_Dataframe = self.Pie_Dataframe[self.Pie_Dataframe[Transaction.ID_Column] != "Discover Payment"]
+        self.Transaction_Color_Dataframe = self.Transaction_Color_Dataframe[self.Transaction_Color_Dataframe[Transaction.ID_Column] != "Discover Payment"]
