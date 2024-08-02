c186291c2804855fdd63bdf5f38b1f15d853759e
Kdot729 <110777824+Kdot729@users.noreply.github.com>
2024-08-02 13:03:48 -0400

    Calling bar component

--- frontend/app/components/analytics.tsx
+++ frontend/app/components/analytics.tsx
@@ -2,11 +2,13 @@ import React from 'react'
 import { Table } from './table/table'
 import { ScrollView } from 'react-native'
 import Pie from './graphs/pie'
+import Bar from './graphs/bar'
 
 const Analytics = () => 
 { 
     return  <ScrollView >
                 <Table />
+                <Bar />
                 <Pie></Pie>
             </ScrollView>
 }
