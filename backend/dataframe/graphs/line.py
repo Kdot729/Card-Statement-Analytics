2f251b54515bcfc36cb5a870bd94a565eadfc7d9
Kdot729 <110777824+Kdot729@users.noreply.github.com>
2024-08-11 18:56:16 -0400

    Grouped transactions per day
    Data will be used for rectangles on the line chart

--- backend/dataframe/graphs/line.py
+++ backend/dataframe/graphs/line.py
@@ -18,7 +18,14 @@ class Line(Graph):
         self.Daily_Sum_Dataframe.columns = [Transaction.Transaction_Date_Column, self.Sum_Column]
         Transaction.Round(self, self.Daily_Sum_Dataframe)
         self.Format_Date(self.Daily_Sum_Dataframe)
-        Dataframe.Convert_Dataframe_to_Dictionary(self, self.Daily_Sum_Dataframe)
+
+        self.Daily_Sum_Dictionary = self.Daily_Sum_Dataframe.set_index(Transaction.Transaction_Date_Column).to_dict('index')
+
+        self.Merge_Color_Dateframe()
+        self.Format_Date(self.Transaction_Color_Dataframe)
+        self.Group_Transactions_Per_Day()
+        self.Merge_Dictionaries()
+
 
     def Merge_Color_Dateframe(self) -> None:
         Graph.Merge_Color_Dateframe(self, self._Dataframe)
@@ -26,3 +33,11 @@ class Line(Graph):
 
     def Format_Date(self, Dataframe: panda.DataFrame) -> None:
         Dataframe[Transaction.Transaction_Date_Column] = panda.to_datetime(Dataframe[Transaction.Transaction_Date_Column], format="%m/%d/%y").dt.strftime('%Y-%m-%d')
+
+    def Group_Transactions_Per_Day(self):
+        Transaction_Dates = self.Transaction_Color_Dataframe[Transaction.Transaction_Date_Column]
+        self.Daily_Grouped_Transactions = {row: self.Transaction_Color_Dataframe[Transaction_Dates == row][self.Lambda_Columns].to_dict(orient='records') for row in Transaction_Dates}
+
+    def Merge_Dictionaries(self):
+        self.Daily_Sum_Dictionary.update({key: [self.Daily_Sum_Dictionary[key], self.Daily_Grouped_Transactions[key]] if key in self.Daily_Grouped_Transactions else self.Daily_Sum_Dictionary[key] for key in self.Daily_Sum_Dictionary})
+        self._Records = [{"Transaction Date": Key, "Sum": Value[0]["Sum"], "Grouped Transactions": Value[1]} for Key,Value in self.Daily_Sum_Dictionary.items()]
