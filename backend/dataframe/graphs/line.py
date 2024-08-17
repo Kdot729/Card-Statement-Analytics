222a4086feb09fdef27acc6163e07345c6dc44bb
Kdot729 <110777824+Kdot729@users.noreply.github.com>
2024-08-17 06:57:53 -0400

    Renamed function name

--- backend/dataframe/graphs/line.py
+++ backend/dataframe/graphs/line.py
@@ -17,12 +17,12 @@ class Line(Graph):
         self.Daily_Sum_Dataframe: panda.DataFrame = Dataframe.Sum_Group_By(self, Transaction.Transaction_Date_Column, Transaction.Amount_Column)
         self.Daily_Sum_Dataframe.columns = [Transaction.Transaction_Date_Column, self.Sum_Column]
         Transaction.Round(self, self.Daily_Sum_Dataframe)
-        self.Format_Date(self.Daily_Sum_Dataframe)
+        self.Convert_Column_to_DateTime(self.Daily_Sum_Dataframe)
 
         self.Daily_Sum_Dictionary = self.Daily_Sum_Dataframe.set_index(Transaction.Transaction_Date_Column).to_dict('index')
 
         self.Merge_Color_Dateframe()
-        self.Format_Date(self.Transaction_Color_Dataframe)
+        self.Convert_Column_to_DateTime(self.Transaction_Color_Dataframe)
         self.Group_Transactions_Per_Day()
         self.Merge_Dictionaries()
 
@@ -31,7 +31,7 @@ class Line(Graph):
         Graph.Merge_Color_Dateframe(self, self._Dataframe)
         self.Transaction_Color_Dataframe = self.Transaction_Color_Dataframe[self.Selected_Columns]
 
-    def Format_Date(self, Dataframe: panda.DataFrame) -> None:
+    def Convert_Column_to_DateTime(self, Dataframe: panda.DataFrame) -> None:
         Dataframe[Transaction.Transaction_Date_Column] = panda.to_datetime(Dataframe[Transaction.Transaction_Date_Column], format="%m/%d/%y").dt.strftime('%Y-%m-%d')
 
     def Group_Transactions_Per_Day(self) -> None:
